4ae348a41fbe8fdce59be10a0704728090befed7
Marvish <mschandr@uci.edu>
2022-07-25 10:39:07 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# Marvish Chandra
+
+
+x = 90,000
+
+if x > 30,000:
+    print("You are eligible to buy a sedan or compact car.")
+if x > 50,000:
+    print("You can buy a mid-end car.")
+if x > 100,000:
+    print("You can buy a luxury car.")
+else:
+    print("Can you tell me how much money you have first?")
